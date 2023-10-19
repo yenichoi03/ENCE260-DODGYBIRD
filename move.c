6d2afdbd8b0c3e36de2e0929b1ef80f7251c19e7
Yeni Choi <ych227@uclive.ac.nz>
2023-10-19 14:47:48 +1300

    Move modules done

--- move.c
+++ move.c
@@ -5,119 +5,89 @@
 #include "ledmat.h"
 #include "move.h"
 
-#define ON 1
-#define OFF 0
 
-
+/* Allows movement of the cannon character in the vertical direction using the North and South navigation switches. */
 void move_cannon(tinygl_point_t *pos_cannon1, tinygl_point_t* pos_cannon2) 
 {
-    /* Can only shoot from rows 0-5*/
 
-    // Check for North button press
     if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-        // Ensure the cannon position is within bounds
         if ((*pos_cannon1).y > 0) {
-            // Clear the previous cannon position
-            tinygl_draw_line(*pos_cannon1, *pos_cannon2, OFF);
-            // Move the cannon up
+            tinygl_draw_line(*pos_cannon1, *pos_cannon2, 0);
             (*pos_cannon1).y--;
             (*pos_cannon2).y--;
-            // Draw the cannon in its new position
-            tinygl_draw_line(*pos_cannon1, *pos_cannon2, ON);
+            tinygl_draw_line(*pos_cannon1, *pos_cannon2, 1);
         }
     }
 
-    // Check for South button press
     if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-        // Ensure the cannon position is within bound
         if ((*pos_cannon1).y < LEDMAT_ROWS_NUM - 2) {
-            // Clear the previous cannon position
-            tinygl_draw_line(*pos_cannon1, *pos_cannon2, OFF);
-            // Move the cannon down
+            tinygl_draw_line(*pos_cannon1, *pos_cannon2, 0);
             (*pos_cannon1).y++;
             (*pos_cannon2).y++;
-            tinygl_draw_line(*pos_cannon1, *pos_cannon2, ON);
+            tinygl_draw_line(*pos_cannon1, *pos_cannon2, 1);
         }
     }
 
 }
 
-
+/* Allows movement of the bird character in all directions using the North, East, South, West navigation switches. */
 void move_bird(void)
 {
-    /* Can only dodge from rows 1-6 */
     static tinygl_point_t pos1 = {1, 4};
     static tinygl_point_t pos2 = {3, 4};
     static tinygl_point_t pos_tip = {2, 3}; 
 
-    // Draw the initial bird position
-    tinygl_draw_line(pos1, pos2, ON);
-    tinygl_draw_point(pos_tip, ON);
+    tinygl_draw_line(pos1, pos2, 1);
+    tinygl_draw_point(pos_tip, 1);
 
-    // Check for North button press
     if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-        // Ensure the bird position is within bounds
         if (pos1.y > 2) {
-            // Clear the previous bird position
-            tinygl_draw_point(pos_tip, OFF);
-            tinygl_draw_line(pos1, pos2, OFF);
-            // Move the bird up
+            tinygl_draw_point(pos_tip, 0);
+            tinygl_draw_line(pos1, pos2, 0);
             pos1.y--;
             pos2.y--;
             pos_tip.y--;
-            // Draw the bird in its new position
-            tinygl_draw_line(pos1, pos2, ON);
-            tinygl_draw_point(pos_tip, ON);
+            tinygl_draw_line(pos1, pos2, 1);
+            tinygl_draw_point(pos_tip, 1);
             
         }
     }
 
-    // Check for South button press
     if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-        // Ensure the bird position is within bounds
         if (pos1.y < LEDMAT_ROWS_NUM - 1) {
-            // Clear the previous bird position
-            tinygl_draw_point(pos_tip, OFF);
-            tinygl_draw_line(pos1, pos2, OFF);
-            // Move the bird down
+            tinygl_draw_point(pos_tip, 0);
+            tinygl_draw_line(pos1, pos2, 0);
             pos1.y++;
             pos2.y++;
             pos_tip.y++;
-            tinygl_draw_line(pos1, pos2, ON);
-            tinygl_draw_point(pos_tip, ON);
+            tinygl_draw_line(pos1, pos2, 1);
+            tinygl_draw_point(pos_tip, 1);
 
         }
     }
 
-    // Check for East button press
     if (navswitch_push_event_p(NAVSWITCH_EAST)) {
-        // Clear the previous bird position
         if (pos2.x < LEDMAT_COLS_NUM - 1) {
-            tinygl_draw_point(pos_tip, OFF);
-            tinygl_draw_line(pos1, pos2, OFF);
-            // Move the bird to the right
+            tinygl_draw_point(pos_tip, 0);
+            tinygl_draw_line(pos1, pos2, 0);
             pos1.x++;
             pos2.x++;
             pos_tip.x++;
-            tinygl_draw_line(pos1, pos2, ON);
-            tinygl_draw_point(pos_tip, ON);
+            tinygl_draw_line(pos1, pos2, 1);
+            tinygl_draw_point(pos_tip, 1);
 
         }
     }
 
-    // Check for West button press
     if (navswitch_push_event_p(NAVSWITCH_WEST)) {
-        // Ensure the bird position is within bounds
         if (pos1.x > LEDMAT_COLS_NUM - 5) {
-            // Clear the previous bird position
-            tinygl_draw_point(pos_tip, OFF);
-            tinygl_draw_line(pos1, pos2, OFF);
-            // Move the bird to the left
+            tinygl_draw_point(pos_tip, 0);
+            tinygl_draw_line(pos1, pos2, 0);
             pos1.x--;
             pos2.x--;
             pos_tip.x--;
-            tinygl_draw_line(pos1, pos2, ON);
-            tinygl_draw_point(pos_tip, ON);
+            tinygl_draw_line(pos1, pos2, 1);
+            tinygl_draw_point(pos_tip, 1);
         }
     }
 }
--- move.h
+++ move.h
@@ -1,29 +1,8 @@
 /**  @file   move.h
      @author Yeni Choi, Mukhriz Khairudin
      @date   14 Oct 2023
-     @brief  LED driver.
+     @brief  Implements the movement of the bird and cannon characters 
 
-    @defgroup LED LED driver
-       
-    This module implements the firing of the cannon
-
-    Here's an example application that turns on a LED:
-
-    @code
-    #include "led.h"
-
-    void main (void)
-    {
-        system_init ();
-        led_init ();
-
-        led_set (LED1, 1);
-
-        while (1)
-        {
-        }
-    }
-    @endcode
 */
 
 #ifndef MOVE_H
@@ -32,7 +11,7 @@
 #include "system.h"
 #include "tinygl.h"
 
-/** Define PIO pins driving LED matrix rows.  */
+/** Defines PIO pins driving LED matrix rows.  */
 static const pio_t rows[] =
 {
     LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO, 
@@ -40,24 +19,21 @@ static const pio_t rows[] =
     LEDMAT_ROW7_PIO
 };
 
-/** Define PIO pins driving LED matrix columns.  */
+/** Defines PIO pins driving LED matrix columns.  */
 static const pio_t cols[] =
 {
     LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
     LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
 };
 
-
-void move_cannon(tinygl_point_t* pos_cannon1, tinygl_point_t* pos_cannon2);
 /** Move the cannon character vertically.
    pos_cannon1 Pointer to the starting position of the cannon.
    pos_cannon2 Pointer to the ending position of the cannon.
 */
-void move_bird(void);
+void move_cannon(tinygl_point_t* pos_cannon1, tinygl_point_t* pos_cannon2);
+
 /* moves the bird character around the screen */
-
-
-
+void move_bird(void);
 
 
 #endif 
